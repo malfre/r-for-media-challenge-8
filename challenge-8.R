b2510fd45fb67dec95ae454446e3e567f0a5dd1c
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-04-29 14:50:50 +0200

    add challenge

---
+++ challenge-8.R
@@ -0,0 +1,8 @@
+# Challenge #8: Inzidenzzahlen der Hamburger Bezirke
+
+# Lade den Datensatz stadtteile_wsg84.RDS
+# Recherchiere die Fallzahlen der letzten sieben Tage für die Hamburger Bezirke
+# https://www.hamburg.de/corona-zahlen/
+# Erstelle eine leaflet Map und visualisiere die Inzidenzzahlen (Achtung: Nicht die Fallzahlen)
+# Nutze dafür Shapes, Legende, Hovereffekte und Labels
+# Exportiere die Map als HTML file
